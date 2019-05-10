885b1d7e0e60564a04617b2af73cd5d730beea98
ly <000000>
2019-05-10 08:50:58 +0800

    life capsule start here

--- src/components/pages/recipient/editPerson.vue
+++ src/components/pages/recipient/editPerson.vue
@@ -2,7 +2,39 @@
   <div>
     <Form>
       <FormItem>
-        <Input v-model="person.recipientName"></Input>
+        <Input v-model="person.personName"></Input>
+      </FormItem>
+      <FormItem>
+        <Tabs :animated="false">
+          <TabPane label="email">
+            <Row>
+              <Col span="22">
+                <Input v-model="person.personName"></Input>
+              </Col>
+              <Col span="2">
+                <span @click="onEmailDel" style="float: right">
+                  <Icon color="red" size="24" type="md-close"/>
+                </span>
+              </Col>
+            </Row>
+            <Button type="primary">add</Button>
+          </TabPane>
+        </Tabs>
+
+      </FormItem>
+      <FormItem>
+        <Tabs :animated="false">
+          <TabPane label="phone">
+            <Input v-model="person.personName"></Input>
+          </TabPane>
+        </Tabs>
+      </FormItem>
+      <FormItem>
+        <Tabs :animated="false">
+          <TabPane label="address">
+            <Input v-model="person.personName"></Input>
+          </TabPane>
+        </Tabs>
       </FormItem>
     </Form>
   </div>
@@ -24,7 +56,11 @@
           personId: this.$store.state.person_id
         }).then((response) => {
           console.log(response)
+          this.person = response.data.data.person
         })
+      },
+      onEmailDel() {
+        console.log('delete email')
       }
     },
     mounted() {
@@ -33,6 +69,7 @@
       if (this.$route.params.personId) {
         this.$store.dispatch('savePersonId', this.$route.params.personId)
       }
+      this.loadAllData()
     }
   }
 </script>
