37729d8c5d69b22e7cbf6661519dbbbbc1a492a7
icesnakeyang <flow_ocean@yahoo.com>
2019-11-10 19:21:17 +0800

    ok

--- src/pages/setttings/settingPage.vue
+++ src/pages/setttings/settingPage.vue
@@ -2,49 +2,56 @@
   <div>
     <Card>
       <p slot="title">
-        用户：{{this.user.nickname}}
+        {{$t('settings.userName')}}：{{this.user.nickname}}
       </p>
-      <p>email:{{this.user.email}}</p>
-      <p>phone:{{this.user.phone}}</p>
+      <p>{{$t('settings.email')}}:{{this.user.email}}</p>
+      <p>{{$t('settings.phone')}}:{{this.user.phone}}</p>
     </Card>
-    <CellGroup @on-click="onCellClick">
-      <Cell name="changeLoginPassword" :title="$t('settings.changeLoginPassword')"/>
-    </CellGroup>
+    <div style="margin-top: 20px; background-color: #dddddd">
+      <CellGroup @on-click="onCellClick">
+        <Cell name="changeLoginPassword" :title="$t('settings.changeLoginPassword')"/>
+      </CellGroup>
+    </div>
+    <div style="margin-top: 20px; background-color: #dddddd">
+      <CellGroup @on-click="onCellClick">
+        <Cell name="changeLoginPassword" :title="$t('settings.changeUser')"/>
+      </CellGroup>
+    </div>
   </div>
 </template>
 
 <script>
-    import {apiGetUserByToken} from "../../api/api";
+  import {apiGetUserByToken} from "../../api/api";
 
-    export default {
-        name: "settingPage",
-        data() {
-            return {
-                user: {}
-            }
-        },
-        methods: {
-            loadAllData() {
-                apiGetUserByToken({}).then((response) => {
-                    if (response.data.code === 0) {
-                        this.user = response.data.data.userInfo
-                    }
-                    console.log(this.user)
-                })
-            },
-            onCellClick(name) {
-                console.log(name)
-                if (name === 'changeLoginPassword') {
-                    this.$router.push({
-                        name: 'changeLoginPassword'
-                    })
-                }
-            }
-        },
-        mounted() {
-            this.loadAllData()
+  export default {
+    name: "settingPage",
+    data() {
+      return {
+        user: {}
+      }
+    },
+    methods: {
+      loadAllData() {
+        apiGetUserByToken({}).then((response) => {
+          if (response.data.code === 0) {
+            this.user = response.data.data.userInfo
+          }
+          console.log(this.user)
+        })
+      },
+      onCellClick(name) {
+        console.log(name)
+        if (name === 'changeLoginPassword') {
+          this.$router.push({
+            name: 'changeLoginPassword'
+          })
         }
+      }
+    },
+    mounted() {
+      this.loadAllData()
     }
+  }
 </script>
 
 <style scoped>
