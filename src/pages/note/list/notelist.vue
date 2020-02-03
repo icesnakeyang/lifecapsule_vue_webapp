c8414fa10412655be5a6bc78dab228d83e593baa
snake <flow_ocean@yahoo.com>
2020-02-03 13:10:34 +0800

    增加日记的翻页功能

--- src/pages/note/list/notelist.vue
+++ src/pages/note/list/notelist.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <div>
-      <note-row v-for="(item, index) in noteList"
-                :key="index"
-                :note="item"
-      ></note-row>
-      <router-view></router-view>
+      <Table :columns="col1" :data="noteList"></Table>
+      <div style="margin-top: 10px;">
+        <Page :total="totalNotes" @on-change="pageChange"/>
+      </div>
     </div>
     <BackTop :height="100" :bottom="50">
       <div>
@@ -24,72 +23,132 @@
 </template>
 
 <script>
-  import {apiListNote} from "@/api/api";
-  import {apiListNoteByCategory} from "@/api/api";
-  import NoteRow from './noteRow'
+    import {apiListNote} from "@/api/api";
+    import {apiListNoteByCategory} from "@/api/api";
+    import NoteRow from './noteRow'
+    import moment from "moment";
 
-  export default {
-    name: "notelist",
-    components: {
-      NoteRow
-    },
-    data() {
-      return {
-        noteList: [],
-        isColor: true,
-        loading: false
-      }
-    },
-    methods: {
-      loadAllData() {
-        /**
-         * 检查当前用户是否有选择的分类，
-         * 如果没有就按时间倒序读取用户最近的笔记
-         */
-        this.loading = true
-        if (this.$store.state.category_id) {
-          //有分类，查询该分类下的笔记
-          apiListNoteByCategory({
-            pageSize: 10,
-            pageIndex: 1,
-            categoryId: this.$store.state.category_id
-          }).then((response) => {
-            if (response.data.code === 0) {
-              this.noteList = response.data.data.noteList
-              this.loading = false
-            } else {
-              this.$Message.error(this.$t("common.loadDataError"))
-              this.loading = false
+    export default {
+        name: "notelist",
+        components: {
+            NoteRow
+        },
+        data() {
+            return {
+                noteList: [],
+                isColor: true,
+                loading: false,
+                pageIndex: 1,
+                pageSize: 10,
+                totalNotes: 0,
+                col1: [
+                    {
+                        title: 'title',
+                        key: 'title'
+                    },
+                    {
+                        title: 'time',
+                        key: 'createdTime',
+                        render: (h, params) => {
+                            return h('span', moment(params.row.createdTime).format('YYYY-MM-DD HH:mm'))
+                        }
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.goDetail(params)
+                                        }
+                                    }
+                                }, 'View'),
+                            ])
+                        }
+                    }
+                ]
             }
-          }).catch((error) => {
-            this.loading = false
-          })
-        } else {
-          //没有分类，按时间倒序读取笔记
-          apiListNote({
-            pageSize: 10,
-            pageIndex: 1
-          }).then((response) => {
-            if (response.data.code === 0) {
-              this.noteList = response.data.data.noteList
-              this.loading = false
-            } else {
-              this.$Message.error(this.$t('common.loadDataError'))
-              this.loading = false
+        },
+        methods: {
+            loadAllData() {
+                /**
+                 * 检查当前用户是否有选择的分类，
+                 * 如果没有就按时间倒序读取用户最近的笔记
+                 */
+                this.loading = true
+                if (this.$store.state.category_id) {
+                    //有分类，查询该分类下的笔记
+                    apiListNoteByCategory({
+                        pageSize: this.pageSize,
+                        pageIndex: this.pageIndex,
+                        categoryId: this.$store.state.category_id
+                    }).then((response) => {
+                        console.log(response)
+                        if (response.data.code === 0) {
+                            this.noteList = response.data.data.noteList
+                            this.totalNotes = response.data.data.totalNote
+                            this.loading = false
+                            console.log(this.noteList)
+                        } else {
+                            this.$Message.error(this.$t("common.loadDataError"))
+                            this.loading = false
+                        }
+                    }).catch((error) => {
+                        this.loading = false
+                    })
+                } else {
+                    //没有分类，按时间倒序读取笔记
+                    apiListNote({
+                        pageSize: 10,
+                        pageIndex: 1
+                    }).then((response) => {
+                        if (response.data.code === 0) {
+                            this.noteList = response.data.data.noteList
+                            this.loading = false
+                        } else {
+                            this.$Message.error(this.$t('common.loadDataError'))
+                            this.loading = false
+                        }
+                    }).catch((error) => {
+                        this.loading = false
+                    })
+                }
+
+            },
+
+            goDetail(params) {
+                this.$store.dispatch('saveNoteId', params.row.noteId)
+                this.$router.push({
+                    name: 'editNote',
+                    params: {
+                        noteId: params.row.noteId
+                    }
+                })
+            },
+
+            pageChange(e) {
+                console.log(e)
+                this.pageIndex = e
+                this.loadAllData()
             }
-          }).catch((error) => {
-            this.loading = false
-          })
+
+        },
+
+        mounted() {
+            this.loadAllData()
         }
-
-      }
-
-    },
-
-    mounted() {
-      this.loadAllData()
     }
-  }
 </script>
 
 <style scoped>
